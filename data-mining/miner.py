67993810195e0db34b20f3bbf9e3d2ef33468043
Anish Sahoo <anishsahoo2005@gmail.com>
2024-05-20 20:52:34 -0400

    Perfected the scraping (I think)

--- data-mining/miner.py
+++ data-mining/miner.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import json
+import re
 
 def get_top_anime_urls(limit=200):
     top_anime_urls = []
@@ -28,6 +29,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
 
+
 def get_anime_details(anime_url):
     response = requests.get(anime_url, headers=headers)
     if response.status_code != 200:
@@ -36,45 +38,148 @@ def get_anime_details(anime_url):
     
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Safely extract the title
-    title_tag = soup.find('h1', class_='title-name h1_bold_none')
-    title = title_tag.find('strong').text.strip() if title_tag and title_tag.find('strong') else 'Unknown Title'
+    # Safely extract the English title
+    english_title_tag = soup.find('p', class_='title-english')
+    english_title = english_title_tag.text.strip() if english_title_tag else None
+    
+    # Safely extract the Japanese title
+    japanese_title_tag = soup.find('h1', class_='title-name h1_bold_none')
+    japanese_title = japanese_title_tag.find('strong').text.strip() if japanese_title_tag and japanese_title_tag.find('strong') else None
+    
+    # Use English title as primary title if available, otherwise use Japanese title
+    title = english_title if english_title else japanese_title if japanese_title else 'Unknown Title'
     
     # Safely extract the score
     score_tag = soup.find('div', class_='score-label')
     score = score_tag.text.strip() if score_tag else 'N/A'
     
     # Safely extract genres
-    genres_tags = soup.find_all('span', itemprop='genre')
-    genres = [genre.text for genre in genres_tags] if genres_tags else []
+    # genres_tags = soup.find_all('span', itemprop='genre')
+    # genres = [genre.text for genre in genres_tags] if genres_tags else []
     
     # Safely extract synopsis
     synopsis_tag = soup.find('p', itemprop='description')
     synopsis = synopsis_tag.text.strip() if synopsis_tag else 'No synopsis available'
+    synopsis = re.sub(r'[\n\r\t]', ' ', synopsis)
+    synopsis = synopsis.split('[Written by MAL Rewrite]')[0].strip()
     
     # Safely extract the studio
-    studio_tag = soup.find('span', class_='information studio')
-    studio = studio_tag.text.strip() if studio_tag else 'Unknown Studio'
+    # studio_tag = soup.find('span', class_='information studio author')
+    # studio = studio_tag.text.strip() if studio_tag else 'Unknown Studio'
+    
+    studio_tag = soup.find('span', class_='information studio author')
+    if studio_tag:
+        studios = studio_tag.text.strip()
+        studios = [studio.strip() for studio in studios.split(',')]
+        studio = ', '.join(studios)
+    else:
+        studio = 'Unknown Studio'
 
     # Safely extract the author
-    author_tag = soup.find('span', class_='information studio author')
-    author = author_tag.text.strip() if author_tag else 'Unknown Author'
+    # author_tag = soup.find('span', class_='information studio author')
+    # author = author_tag.text.strip() if author_tag else 'Unknown Author'
 
     # Safely extract the type (movie, TV series, etc.)
     type_tag = soup.find('span', class_='information type')
     type = type_tag.text.strip() if type_tag else 'Unknown Type'
 
+    theme_element = soup.find('span', text='Themes:')
+    genre_element = soup.find('span', text='Genres:')
+    
+    # genres_tag = soup.find('span', text='Genres:')
+    # if genres_tag:
+    #     genres = [a.text.strip() for a in genres_tag.find_next_siblings('a')]
+    #     genres = ', '.join(genres)
+    # else:
+    #     genres = 'Unknown Genres'
+
+    # print(f'Genres: {genres}')
+
+    
+    demographic_element = soup.find('span', text='Demographic:')
+
+    # Extract the theme, genre, and demographic values
+    theme = ', '.join([a.text.strip() for a in theme_element.find_next_siblings('a')]) if theme_element else 'Unknown'
+    genre = ', '.join([a.text.strip() for a in genre_element.find_next_siblings('a')]) if genre_element else 'Unknown'
+    demographic = demographic_element.find_next('a').text.strip() if demographic_element else 'Unknown'
+
     print(f'Fetched details for {title}')
+    # print(json.dumps({
+    #     'title': title,
+    #     'english_title': english_title,
+    #     'japanese_title': japanese_title,
+    #     'score': score,
+    #     'genres': genre,
+    #     'themes': theme,
+    #     'demographic': demographic,
+    #     'synopsis': synopsis,
+    #     'url': anime_url,
+    #     'studio': studio,
+    #     'type': type
+    # }, indent=2))
+
     return {
         'title': title,
+        'english_title': english_title,
+        'japanese_title': japanese_title,
         'score': score,
-        'genres': genres,
+        'genres': genre,
+        'themes': theme,
+        'demographic': demographic,
         'synopsis': synopsis,
         'url': anime_url,
         'studio': studio,
-        'author': author,
         'type': type
     }
+
+
+# def get_anime_details(anime_url):
+#     response = requests.get(anime_url, headers=headers)
+#     if response.status_code != 200:
+#         print(f"Failed to fetch {anime_url}: Status code {response.status_code}")
+#         return None
+    
+#     soup = BeautifulSoup(response.content, 'html.parser')
+    
+#     # Safely extract the title
+#     title_tag = soup.find('h1', class_='title-name h1_bold_none')
+#     title = title_tag.find('strong').text.strip() if title_tag and title_tag.find('strong') else 'Unknown Title'
+    
+#     # Safely extract the score
+#     score_tag = soup.find('div', class_='score-label')
+#     score = score_tag.text.strip() if score_tag else 'N/A'
+    
+#     # Safely extract genres
+#     genres_tags = soup.find_all('span', itemprop='genre')
+#     genres = [genre.text for genre in genres_tags] if genres_tags else []
+    
+#     # Safely extract synopsis
+#     synopsis_tag = soup.find('p', itemprop='description')
+#     synopsis = synopsis_tag.text.strip() if synopsis_tag else 'No synopsis available'
+    
+#     # Safely extract the studio
+#     studio_tag = soup.find('span', class_='information studio')
+#     studio = studio_tag.text.strip() if studio_tag else 'Unknown Studio'
+
+#     # Safely extract the author
+#     author_tag = soup.find('span', class_='information studio author')
+#     author = author_tag.text.strip() if author_tag else 'Unknown Author'
+
+#     # Safely extract the type (movie, TV series, etc.)
+#     type_tag = soup.find('span', class_='information type')
+#     type = type_tag.text.strip() if type_tag else 'Unknown Type'
+
+#     print(f'Fetched details for {title}')
+#     return {
+#         'title': title,
+#         'score': score,
+#         'genres': genres,
+#         'synopsis': synopsis,
+#         'url': anime_url,
+#         'studio': studio,
+#         'author': author,
+#         'type': type
+#     }
 
 top_anime_urls = get_top_anime_urls()
 anime_details_list = []
